d2044e770427eb984a44e5db483e93ea49120440
DNM03 <113103124+DNM03@users.noreply.github.com>
2023-06-05 22:53:51 +0700

    Update EditProfileFragment.java
    update lib

--- app/src/main/java/com/example/friendverse/Fragment/EditProfileFragment.java
+++ app/src/main/java/com/example/friendverse/Fragment/EditProfileFragment.java
@@ -21,6 +21,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
