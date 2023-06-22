f82b63086ad5a26bb258740533a825124bdbf83a
QuocBaoKho <113084141+QuocBaoKho@users.noreply.github.com>
2023-06-22 23:21:28 +0700

    Update PostFragment.java

--- app/src/main/java/com/example/friendverse/Fragment/PostFragment.java
+++ app/src/main/java/com/example/friendverse/Fragment/PostFragment.java
@@ -48,7 +48,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
-import com.theartofdev.edmodo.cropper.CropImage;
+//import com.theartofdev.edmodo.cropper.CropImage;
 
 import org.jetbrains.annotations.NotNull;
 
