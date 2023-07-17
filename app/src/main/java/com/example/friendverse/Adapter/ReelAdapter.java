8490ef0052a49a35cf5d564b12f322646e7827c0
tuonghuynh11 <89913828+tuonghuynh11@users.noreply.github.com>
2023-07-17 14:34:02 +0700

    Update ReelAdapter.java

--- app/src/main/java/com/example/friendverse/Adapter/ReelAdapter.java
+++ app/src/main/java/com/example/friendverse/Adapter/ReelAdapter.java
@@ -15,6 +15,7 @@ import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
